b13b4653f9f4d38c2aaa473c54f8643e7d1b7064
Saeid HA <sdhosseinabadi@gmail.com>
2022-11-27 16:56:24 +0000

    Update sui_command2.sh

---
+++ sui_command2.sh
@@ -0,0 +1,13 @@
+cd /
+sui client active-address | tee faucet.txt
+
+git clone https://github.com/MystenLabs/sui.git --branch devnet
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+cp crates/sui-config/data/fullnode-template.yaml fullnode.yaml
+curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+cd sui
+screen -S sui
+# cargo run --release --bin sui-node -- --config-path fullnode.yaml
+cat faucet.txt
