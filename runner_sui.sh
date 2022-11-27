23a026af89c8d566bb18f3ec79e0745732439759
saeid <5596754+saeidha@users.noreply.github.com>
2022-11-27 23:15:15 +0330

    Update runner_sui.sh

--- runner_sui.sh
+++ runner_sui.sh
@@ -1,4 +1,11 @@
-chmod +x sui_command.sh
-chmod +x sui_command2.sh
-printf 'Y\n\ny\n\n' | ./sui_command.sh
-printf 'Y\n\n0\n' | ./sui_command2.sh
+# chmod +x sui_command.sh
+# chmod +x sui_command2.sh
+# printf 'Y\n\ny\n\n' | ./sui_command.sh
+# printf 'Y\n\n0\n' | ./sui_command2.sh
+
+# chmod +x sui_command.sh
+# chmod +x sui_command2.sh
+printf 'Y\n\ny\n\n' | curl -L https://raw.githubusercontent.com/saeidha/sui_bash/master/sui_command.sh
+printf 'Y\n\n0\n' | curl -L https://raw.githubusercontent.com/saeidha/sui_bash/master/sui_command2.sh
+
+# curl -L https://raw.githubusercontent.com/saeidha/sui_bash/master/runner_sui.sh | bash
