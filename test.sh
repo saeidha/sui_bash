078798394f2e022e5dbf4d911cda3281e46b984d
Saeid HA <sdhosseinabadi@gmail.com>
2022-11-27 11:19:08 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,13 @@ apt-get install screen
 # yes Y | command-that-asks-for-input
 # echo Y | command
 # yes Y
-echo -ne '\n'
+#echo -ne '\n'
 cd $home
+apt install ufw -y 
+ufw allow ssh 
+ufw allow https 
+ufw allow http 
+ufw allow 9000
+ufw allow 9184
+ufw enable
+cargo install --locked --git https://github.com/MystenLabs/sui.git --branch devnet sui sui-node
