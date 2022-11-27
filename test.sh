057676f4fd4da59dd2d7d33af90bffd3c17cc134
Saeid HA <sdhosseinabadi@gmail.com>
2022-11-27 11:14:45 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 source $HOME/.cargo/env
 rustup update
 apt-get install screen
-yes Y | command-that-asks-for-input
+# yes Y | command-that-asks-for-input
 # echo Y | command
-yes
+yes Y
 echo -ne '\n'
 cd $home
