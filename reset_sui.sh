3ed8957f9cef10f5b8e47a032feeb6f2bcaf2ad0
saeid <5596754+saeidha@users.noreply.github.com>
2022-12-02 14:50:17 +0330

    Update reset_sui.sh

--- reset_sui.sh
+++ reset_sui.sh
@@ -5,5 +5,4 @@ rm -r suidb genesis.blob
 git fetch upstream
 git checkout -B devnet --track upstream/devnet
 wget https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-cargo run --release --bin sui-node -- --config-path fullnode.yaml
-screen -D -RR
+screen -d -m cargo run --release --bin sui-node -- --config-path fullnode.yaml
