75a3d535548478c1924927b9cbdf513acc9af37b
saeid <5596754+saeidha@users.noreply.github.com>
2022-12-02 13:03:08 +0330

    Create reset_sui.sh
    add reset_sui

---
+++ reset_sui.sh
@@ -0,0 +1,9 @@
+sudo apt-get install xdotool
+screen -S sui
+cd $HOME/sui
+rm -r suidb genesis.blob
+git fetch upstream
+git checkout -B devnet --track upstream/devnet
+wget https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+cargo run --release --bin sui-node -- --config-path fullnode.yaml
+xdotool key ctrl+alt+d
