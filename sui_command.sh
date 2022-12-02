52eedcd8b8455ca375521e449b9c54ba7ab9f1c7
saeid <5596754+saeidha@users.noreply.github.com>
2022-12-02 14:55:47 +0330

    Update sui_command.sh

--- sui_command.sh
+++ sui_command.sh
@@ -25,15 +25,19 @@ ufw allow 9184
 ufw enable
 cargo install --locked --git https://github.com/MystenLabs/sui.git --branch devnet sui sui-node
 cargo install --git https://github.com/move-language/move move-analyzer --features "address20"
-# sui client active-address | tee faucet.txt
 
-# git clone https://github.com/MystenLabs/sui.git --branch devnet
-# cd sui
-# git remote add upstream https://github.com/MystenLabs/sui
-# git fetch upstream
-# cp crates/sui-config/data/fullnode-template.yaml fullnode.yaml
-# curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-# cd sui
-# screen -S sui
-# # cargo run --release --bin sui-node -- --config-path fullnode.yaml
-# cat faucet.txt
+cd $home
+sleep 5s
+sui client active-address | tee faucet.txt
+sleep 5s
+git clone https://github.com/MystenLabs/sui.git --branch devnet
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+cp crates/sui-config/data/fullnode-template.yaml fullnode.yaml
+curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+cd sui
+screen -S sui
+# cargo run --release --bin sui-node -- --config-path fullnode.yaml
+cd $home
+cat faucet.txt
