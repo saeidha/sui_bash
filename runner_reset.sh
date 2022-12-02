458a9409518c9827b2065cc39a5ff4111745c3db
saeid <5596754+saeidha@users.noreply.github.com>
2022-12-02 14:52:25 +0330

    Update runner_reset.sh

--- runner_reset.sh
+++ runner_reset.sh
@@ -1,5 +1,5 @@
-yes | sudo apt-get install xdotool
 chmod +x reset_sui.sh
+chmod +x reset_sui.service
 cp reset_sui.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/reset_sui.service
 systemctl enable reset_sui.service
